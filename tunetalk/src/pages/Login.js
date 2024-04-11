0f38d7d0fee35e488ab5e7584da2ef42776ddf20
RedOwl <66703097+RedOwlllll@users.noreply.github.com>
2024-04-11 22:03:11 +1200

    updated commit?

--- tunetalk/src/pages/Login.js
+++ tunetalk/src/pages/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "../css/App.css"; // NOTE: put 2 . ("..") since this file is in it's own folder too. 
+// test server
+import { useState, useEffect } from "react";
+import {getTest} from "./functions/test";
 
 function Login() {
+    const [data, setData] = useState("Login");
+
+    useEffect(() => {
+        getTest()
+            .them((res) => {
+            setData(res.message);
+            })
+            .catch(err => console.log(err));
+    }, []);
+    
     return (
         <div className="home-page">
             <h1>Login</h1> 
