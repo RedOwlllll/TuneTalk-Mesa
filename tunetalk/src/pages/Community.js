e3ed2535b999c32bdec3c7a8d5c00c17535412b1
RedOwl <66703097+RedOwlllll@users.noreply.github.com>
2024-04-18 23:13:34 +1200

    genre pages

--- tunetalk/src/pages/Community.js
+++ tunetalk/src/pages/Community.js
@@ -4,7 +4,19 @@ import "../css/App.css"; // NOTE: put 2 . ("..") since this file is in it's own 
 function Community() {
     return (
         <div className="community-page">
-            <h1>Community</h1>  
+            <h1>Music Genres</h1>
+            <ul>
+                <li><Link to="/pop">Pop</Link></li>
+                <li><Link to="/k-pop">K-pop</Link></li>
+                <li><Link to="/rock">Rock</Link></li>
+                <li><Link to="/rnb">R&B</Link></li>
+                <li><Link to="/indie">Indie</Link></li>
+                <li><Link to="/hip-hop">Hip-hop</Link></li>
+                <li><Link to="/country">Country</Link></li>
+                <li><Link to="/electronic">Electronic</Link></li>
+                <li><Link to="/metal">Metal</Link></li>
+                <li><Link to="/classical">Classical</Link></li>
+            </ul>  
         </div>
     );
 }
