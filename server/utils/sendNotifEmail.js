46e590322e50501a678b9929d08b747405ae6290
tley4 <105393760+tley4@users.noreply.github.com>
2024-04-20 19:42:50 +1200

    Updated sendNotifEmail to fetch user's email from DB

--- server/utils/sendNotifEmail.js
+++ server/utils/sendNotifEmail.js
@@ -1,21 +1,29 @@
+const UserDetails = require("../models/UserDetails");
 const { NotificationTransporter } = require("./NotificationTransporter");
 
-const sendNotifEmail = (user) => {
+const sendNotifEmail = async () => {
     const transporter = NotificationTransporter(); //creating mail transporter object
+
+    const user = await UserDetails.findOne({email}); //fetch the user's email address to send the email to
+
+    if (!user)
+    {
+        console.log('The user does not exist');
+    }
 
     const mailOptions = {
         from: '"TuneTalk" <skk8822@autuni.ac.nz>',
-        to: "tabithakay1004@gmail.com",
+        to: user.email,
         subject: "!TIME TO TUNE IN!",
         html: `<p>Hello! Click the link below to post your song whether it's currently playing or recently played!</p>
             <a href = 'http://192.168.1.68:3000'>Post your song of the day</a>
         `,
     };
 
-    transporter.sendMail(mailOptions, (Error, info) => {
-        if(Error)
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error)
         {
-            console.log(Error);
+            console.log("Error sending notification email", error);
         }
         else
         {
