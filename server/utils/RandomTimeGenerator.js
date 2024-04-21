433f08ad856156a2ac3f45671d8b724773e54ad7
tley4 <105393760+tley4@users.noreply.github.com>
2024-04-21 13:48:51 +1200

    Change the email prompt

--- server/utils/RandomTimeGenerator.js
+++ server/utils/RandomTimeGenerator.js
@@ -37,6 +37,6 @@ console.log(`Notification will be sent at ${hour}:${min}:${sec}`);
 
 // Check if the generated time matches the current time
 if (hour === new Date().getHours() && min === new Date().getMinutes() && sec === new Date().getSeconds()) {
-    console.log("Sending notification email now...");
+    console.log("Loading the email...");
     sendNotifEmail(UserDetails);
 }
