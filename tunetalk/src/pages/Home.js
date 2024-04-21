10b2940c36c659b8a214b97a6fcc77bce089bceb
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-04-21 21:52:10 +1200

    commit 2

--- tunetalk/src/pages/Home.js
+++ tunetalk/src/pages/Home.js
@@ -1,47 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import React from "react";
 import "../css/App.css"; // NOTE: put 2 . ("..") since this file is in it's own folder too. 
+import Post from "./Post";
 
 export const Home = () => {
     return (
         <div className="home-page">
-            <h1>Home</h1>
-            {!token ?
-                <a href={getLoginURL()}>Login to Spotify</a>
-                : <button onClick={logout}>Logout</button>}
-
-            <div className="button-container">
-                <div className="button-box">
-                    <button onClick={getRecentTrack}>Show Last Played Song</button>
-                </div>
-            </div>
-
-            {/* Display the recent track information */}
-            {recentTrack && (
-                <div className="post-card">
-                    <div className="post-card-content">
-                        <h2 className="song-title">{recentTrack.title}</h2>
-                        <h3 className="artist-name">{recentTrack.artist}</h3>
-                    </div>
-                    <div className="post-card-image-container">
-                        <img src={recentTrack.albumCover} alt={`${recentTrack.title} Album Cover`} className="post-card-image" />
-                        <StarRating onRating={(rate) => console.log(rate)} />
-                    </div>
-                    <div className="post-card-content">
-                        {/* Comment form */}
-                        <form onSubmit={handleCommentSubmit}>
-                            <input
-                                type="text"
-                                className="comment-input"
-                                placeholder="Add a comment..."
-                                value={comment}
-                                onChange={(e) => setComment(e.target.value)}
-                            />
-                            <button type="submit" className="submit-comment">Post</button>
-                        </form>
-                    </div>
-                </div>
-            )}
+            <h1>Home</h1> 
+            <Post />
         </div>
     );
 }
