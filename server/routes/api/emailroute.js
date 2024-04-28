34bc09d422bc2b2c055d5b92f1e35d11f5ddf3ce
tley4 <105393760+tley4@users.noreply.github.com>
2024-04-28 16:07:51 +1200

    Added the SendEmailNotification function

--- server/routes/api/emailroute.js
+++ server/routes/api/emailroute.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/UserDetails');
+const UserDetails = require('../../models/UserDetails');
+const { SendNotificationEmail } = require('../../utils/SendEmailNotification');
 
+router.get('/email', async(req, res) => {
+    try{
+        const userId = req.user.id;
+        const user = await UserDetails.findById(userId);
+
+        if(!user)
+        {
+            return res.status(404).json({error: "User not found"});
+        }
+
+        //Send notification email
+        SendNotificationEmail(user);
+
+        res.json({ email: user.email })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+module.exports = router;
