a0a005e4af9f02b4352467a236890bab387173c6
tley4 <105393760+tley4@users.noreply.github.com>
2024-04-28 21:21:41 +1200

    Created a sendNotificationEmail function

--- server/routes/api/tunetalklogin.js
+++ server/routes/api/tunetalklogin.js
@@ -5,11 +5,32 @@ const bcrypt = require("bcryptjs"); // using bycryptjs to encrypt passwords
 const JWT = require('jsonwebtoken') // using jsonwebtoken library
 const JWT_SECRET = "fghsdf123"; // secret key used to verify the json webtokens (note should be an env file, but because this is being marked, would be easier to not include in a env file). 
 const user = require("../../models/UserDetails"); // import user details model
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(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);
 
 // Function to validate email format with the correct pattern regex
 function emailRegex(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
+}
+
+function sendNotificationEmail(userEmail) {
+    const notification = {
+        to: userEmail,
+        from: '2024tunetalk@gmail.com',
+        subject: '!TIME TO TUNE IN',
+        text: 'Hello! It is time to post your current/recently played song for your friends to see!'
+    };
+
+    sgMail
+        .send(notification)
+        .then(() => {
+            console.log('Notification Email sent');
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 router.post("/", async(req,res) => {
@@ -39,6 +60,9 @@ router.post("/", async(req,res) => {
             // will generate a token with the user id in mongodb as well as their email and pass the jwt_secret variable 
             const token = JWT.sign({ id: existingUser.id, email: existingUser.email}, JWT_SECRET); 
             
+            // Send email notification
+            sendNotificationEmail(existingUser.email);
+
             // Return user's email along with token
             return res.json({status: "ok", user: {email: existingUser.email, username: existingUser.username}, token });
         }
