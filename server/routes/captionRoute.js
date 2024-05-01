3c2697fb6bb1465a960da23bde0ec291e43622e6
tley4 <105393760+tley4@users.noreply.github.com>
2024-05-01 17:30:13 +1200

    Create captionRoute.js

---
+++ server/routes/captionRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Caption = require('../models/caption');
+
+router.post('/save-caption', (req, res) => {
+    const { caption } = req.body;
+
+    const newCaption = new Caption({ caption });
+    newCaption.save()
+        .then(savedCaption => {
+            res.status(200).json(savedCaption);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'Error saving caption to database' });
+        });
+});
+
+module.exports = router;
