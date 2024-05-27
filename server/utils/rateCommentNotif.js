2e0041457fd323db837af367bc35861a417dcd33
tley4 <105393760+tley4@users.noreply.github.com>
2024-05-27 20:14:53 +1200

    Create rateCommentNotif.js

---
+++ server/utils/rateCommentNotif.js
@@ -0,0 +1,45 @@
+const sgMail = require('@sendgrid/mail');
+const notifier = require('node-notifier');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+sgMail.setApiKey(process.env.API_KEY);
+
+//To the send the email notification to the user
+function rateComNotifEmail(userEmail, username, friend, comment) {
+    const notification = {
+        to: userEmail,
+        from: {
+            name:'Tune Talk',
+            email: '2024tunetalk@gmail.com'
+        },
+        subject: '!TIME TO TUNE IN!',
+        text: 'It is time to post your current/recently played song for your friends to see!',
+        html: `<h2>Hello ${username}!</h2><p>${friend}: ${comment}!
+        <br><br><a href="http://localhost:3000/account/feed">Post here</a></p>`
+    };
+
+    sgMail
+        .send(notification)
+        .then(() => {
+            console.log('Notification Email sent');
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+//To push a notification to the user's device 
+function pushRateCommentNotif() {
+    notifier.notify({
+        appName: 'Tune Talk',
+        title: '!TIME TO TUNE IN!',
+        message: "Your friends have commented and rated your post!",
+        icon: path.join('http://localhost:3000/static/media/TuneTalkLogoBlack.16d0f5c9352a06b53052641b8fab2fac.svg')
+    });
+}
+
+module.exports = { rateComNotifEmail, pushRateCommentNotif };
